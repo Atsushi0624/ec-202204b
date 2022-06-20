62560da644f4f819b3a4b900ce2d2421ddb28eff
Atsushi0624 <s.s_sugar_r.t@me.com>
2022-06-20 14:30:00 +0900

    OrderToppingドメインの作成

--- src/main/java/com/example/domain/OrderTopping.java
+++ src/main/java/com/example/domain/OrderTopping.java
@@ -1,5 +1,21 @@
 package com.example.domain;
 
-public class OrderTopping {
+import lombok.Data;
 
+/**
+ * OrderToppingを表すドメイン.
+ * 
+ * @author atsushi.kikuchi
+ *
+ */
+@Data
+public class OrderTopping {
+	/** ID */
+	private Integer id;
+	/** トッピングID */
+	private Integer toppingId;
+	/** 注文アイテムID */
+	private Integer orderItemId;
+	/** トッピング情報 */
+	private Topping topping;
 }
