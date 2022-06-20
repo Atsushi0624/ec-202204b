e839a8b208f771da746fa80e360b420dd37d7c10
Atsushi0624 <s.s_sugar_r.t@me.com>
2022-06-20 14:26:40 +0900

    OrderItemドメインの作成

--- src/main/java/com/example/domain/OrderItem.java
+++ src/main/java/com/example/domain/OrderItem.java
@@ -1,5 +1,48 @@
 package com.example.domain;
 
-public class OrderItem {
+import java.sql.Timestamp;
+import java.util.Date;
+import java.util.List;
 
+import lombok.Data;
+
+/**
+ * OrderItemを表すドメイン.
+ * 
+ * @author atsushi.kikuchi
+ *
+ */
+@Data
+public class OrderItem {
+	
+	/** ID */
+	private Integer id;
+	/** 顧客ID */
+	private Integer customerId;
+	/** 注文状態 */
+	private Integer status;
+	/** 合計金額 */
+	private Integer totalPrice;
+	/** 注文日 */
+	private Date orderDate;
+	/** 宛先氏名 */
+	private String destinationName;
+	/** 宛先Eメール */
+	private String destinationEmail;
+	/** 宛先郵便番号 */
+	private String destinationZipcode;
+	/** 宛先住所 */
+	private String destinationAddress;
+	/** 宛先TEL */
+	private String destinationTel;
+	/** 配達時間 */
+	private Timestamp deriveryTime;
+	/** 支払い方法 */
+	private Integer paymentMethod;
+	/** 顧客情報 */
+	private Customer customer;
+	/** 支払い方法 */
+	private List<OrderItem> orderItemList;
+	
+	
 }
