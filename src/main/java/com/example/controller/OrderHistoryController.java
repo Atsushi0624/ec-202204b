5046163f35d43a079212a7d1c3c8ef74ef0598a0
Atsushi0624 <s.s_sugar_r.t@me.com>
2022-06-22 12:08:42 +0900

    OrderRepositoryに注文一覧を取得するメソッドを追加

---
+++ src/main/java/com/example/controller/OrderHistoryController.java
@@ -0,0 +1,14 @@
+package com.example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("")
+public class OrderHistoryController {
+
+	@RequestMapping("/showOrderHistory")
+	public String showOrderHistory() {
+		return "order_history";
+	}
+}
