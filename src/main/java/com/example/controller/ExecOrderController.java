baf03a4524240365226dc78f6ccca2e2f1f9caf0
Nao-Y1996 <0809n.ysoccer@gmail.com>
2022-06-23 10:58:38 +0900

    注文フォームのエラーメッセージ表示の位置を修正

--- src/main/java/com/example/controller/ExecOrderController.java
+++ src/main/java/com/example/controller/ExecOrderController.java
@@ -63,7 +63,7 @@ public class ExecOrderController {
 	public String execOrder(@Validated ExecOrderForm form, BindingResult result, Model model){
 		LocalDateTime deliveryTime = null;
 		System.out.println(form);
-		if (!form.getDeliveryTimeList().contains("")) { // form.getDeliveryTimeList()は日にちと時間がList<String>で入っているので両方入っているか確認する
+		if (!form.getDeliveryTimeList().contains("") && ! (form.getDeliveryTimeList().size()==0)) { // form.getDeliveryTimeList()は日にちと時間がList<String>で入っているので両方入っているか確認する
 			// 配達日時のチェック
 			String strDeliveryTime = form.getDeliveryTimeList().get(0) + " " + form.getDeliveryTimeList().get(1) +":00:00";
 			deliveryTime = LocalDateTime.parse(strDeliveryTime, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
