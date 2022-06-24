81c5b7c88e441b4eee33f825ba148356e00a7a86
AmenophisI <sessyomaru427@gmail.com>
2022-06-24 12:51:49 +0900

    セレクトボックスからリクエストパラメータが送られるようにしました

--- src/main/java/com/example/controller/AdminAnalysisController.java
+++ src/main/java/com/example/controller/AdminAnalysisController.java
@@ -1,9 +1,20 @@
 package com.example.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequestMapping("/adminAnalysis")
+import com.example.domain.Item;
+import com.example.service.ItemListService;
+
+@Controller
+@RequestMapping("")
 public class AdminAnalysisController {
+	@Autowired
+	private ItemListService itemListService;
 
 	/**
 	 * 商品別円グラフを表示.
@@ -12,5 +23,21 @@ public class AdminAnalysisController {
 	 */
 	public String displayPieChart() {
 		return "pie_chart";
+	}
+
+	@RequestMapping("/ranking")
+	public String displayRanking(Model model, String ageKey, String genderKey) {
+		List<Item> itemList = null;
+		if (ageKey == null) {
+			itemList = itemListService.findAll();
+			model.addAttribute("rankingKey", "10");
+		} else if (ageKey.equals("20")) {
+			model.addAttribute("rankingKey", "10");
+		} else if (ageKey.equals("30")) {
+			model.addAttribute("rankingKey", "10");
+		}
+		int rank = 0;
+		model.addAttribute("itemList", itemList);
+		return "ranking";
 	}
 }
